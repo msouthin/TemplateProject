acf8f88d49dd800a4187bfc5a653651907f5077b
Mathew Southin <msouthin@Ms-MacBook-Pro.local>
2016-10-18 15:37:51 -0700

    Add permission view with all permissions

--- TemplateProject/TemplateProject/PermissionViewController.swift
+++ TemplateProject/TemplateProject/PermissionViewController.swift
@@ -7,30 +7,57 @@
 //
 
 import UIKit
+import PAPermissions
 
-class PermissionViewController: PAPPermissionsViewController {
+class PermissionViewController: PAPermissionsViewController {
 
-    let microphoneCheck = PAMicrophonePermissionsCheck()
+    let addressBookCheck = PAABAddressBookCheck()
+    let bluetoothCheck = PABluetoothPermissionsCheck()
+    let calendarCheck = PACalendarPermissionsCheck()
     let cameraCheck = PACameraPermissionsCheck()
+    let contactsCheck = PACNContactsPermissionsCheck()
+    let eventsCheck = PAEKPermissionsCheck()
+    let locationCheck = PALocationPermissionsCheck()
+    let microphoneCheck = PAMicrophonePermissionsCheck()
+    let motionFitnessCheck = PAMotionFitnessPermissionsCheck()
+    let notificationsCheck = PANotificationsPermissionsCheck()
+    let photoLibraryCheck = PAPhotoLibraryPermissionsCheck()
+    let remindersCheck = PARemindersPermissionsCheck()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Custom settings
-        self.locationCheck.requestAlwaysAuthorization = true
-        
-        
         let permissions = [
+            PAPermissionsItem.itemForType(.bluetooth, reason: "Required to shoot awesome photos")!,
+            PAPermissionsItem.itemForType(.camera, reason: "Required to shoot awesome photos")!,
+            PAPermissionsItem.itemForType(.calendar, reason: "Required to hear your beautiful voice")!,
+            PAPermissionsItem.itemForType(.contacts, reason: "Required to hear your beautiful voice")!,
+            PAPermissionsItem.itemForType(.location, reason: "Required to hear your beautiful voice")!,
             PAPermissionsItem.itemForType(.microphone, reason: "Required to hear your beautiful voice")!,
-            PAPermissionsItem.itemForType(.camera, reason: "Required to shoot awesome photos")!]
+            PAPermissionsItem.itemForType(.motionFitness, reason: "Required to hear your beautiful voice")!,
+            PAPermissionsItem.itemForType(.notifications, reason: "Required to hear your beautiful voice")!,
+            PAPermissionsItem.itemForType(.photoLibrary, reason: "Required to hear your beautiful voice")!,
+            PAPermissionsItem.itemForType(.reminders, reason: "Required to hear your beautiful voice")!
+        ]
         
         let handlers = [
+            PAPermissionsType.bluetooth.rawValue: self.microphoneCheck,
+            PAPermissionsType.camera.rawValue: self.cameraCheck,
+            PAPermissionsType.calendar.rawValue: self.calendarCheck,
+            PAPermissionsType.contacts.rawValue: self.contactsCheck,
+            PAPermissionsType.location.rawValue: self.locationCheck,
             PAPermissionsType.microphone.rawValue: self.microphoneCheck,
-            PAPermissionsType.camera.rawValue: self.cameraCheck]
+            PAPermissionsType.motionFitness.rawValue: self.motionFitnessCheck,
+            PAPermissionsType.notifications.rawValue: self.notificationsCheck,
+            PAPermissionsType.photoLibrary.rawValue: self.photoLibraryCheck,
+            PAPermissionsType.reminders.rawValue: self.remindersCheck
+        ]
+        
         self.setupData(permissions, handlers: handlers)
         
         self.titleText = "My Awesome App"
         self.detailsText = "Please enable the following"
+        
     }
 
 
